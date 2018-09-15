21ac8eedb11c76dd642c01f193c6bf1299850158
unknown <eyalschefler@gmail.com>
2018-09-15 14:58:30 +0300

    fixed all bugs inside the scripts

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+sudo -u vagrant /bin/sh <<\VAGRANT_BLOCK
+
+yes | ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/id_rsa_server1.pub
+
+VAGRANT_BLOCK
+
+echo "    StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo "    UserKnownHostsFile=/dev/null" >> /etc/ssh/ssh_config
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+cat /vagrant/id_rsa_server1.pub >> .ssh/authorized_keys
+rm /vagrant/id_rsa_server1.pub
